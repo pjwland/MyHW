9c14f37bd8b984f9824c3952643638cb72565241
PJW <pjwland@skuniv.ac.kr>
2018-06-15 02:21:09 +0900

    first version

---
+++ src/B.java
@@ -0,0 +1,7 @@
+package SE;
+
+public class B {
+	public static void main(String[] args){
+		System.out.println("B");
+	}
+}
---
+++ src/C.java
@@ -0,0 +1,7 @@
+package SE;
+
+public class C {
+	public static void main(String[] args){
+		System.out.println("C");
+	}
+}
